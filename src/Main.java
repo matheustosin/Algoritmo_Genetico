de15228f2a2e4d276879d7ab7cf34e4529b15194
Matheus <matheus.tosin@edu.pucrs.br>
2021-04-19 16:59:11 -0300

    inicio projeto, leitura testes txt

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,42 @@ import java.io.*;
 
 public class Main {
     private static final int TAM = 11;
-    private static File dir = new File("C:\\Users\\matheus.tosin\\Desktop\\PUCRS\\IA\\T1");
-    private static  File arq = new File(dir, "duplos4.txt");
-    private static int[][] preferencias;
+    private static File dir = new File("C:\\Users\\matheus.tosin\\Desktop\\PUCRS\\IA\\Algoritmo_Genetico");
+    private static  File arq = new File(dir, "duplos50.txt");
+    private static String[][] preferencias;
 
     public static void main(String[] args) throws FileNotFoundException {
         carregaTxt(arq);
+
+        printMatriz();
+    }
+
+    public static void printMatriz() {
+        for(int i = 0; i < (preferencias.length); i++) {
+            for(int j = 0; j < (preferencias.length/2)+1; j++) {
+                System.out.print(preferencias[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void carregaTxt(File arq) throws FileNotFoundException {
         try {
-
-        FileReader fileReader = new FileReader(arq);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String linha = "";
-        while ( ( linha = bufferedReader.readLine() ) != null) {
-            for (int i=0; i < 8; i++) {
-                for (int j=0; j < 5; j++) {
-                    preferencias[i] = linha;
+            int lin = 0;
+            FileReader fileReader = new FileReader(arq);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linha = "";
+            int tamanho = Integer.parseInt(bufferedReader.readLine());
+            preferencias = new String[tamanho*2][tamanho+1];
+                while ( ( linha = bufferedReader.readLine() ) != null) {
+                    String[] dados = linha.split(" ");
+                    for(int i = 0; i < dados.length; i++) {
+                        preferencias[lin][i] = dados[i];
+                    }
+                    lin++;
                 }
-        }
-        fileReader.close();
-        bufferedReader.close();
+                fileReader.close();
+                bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
