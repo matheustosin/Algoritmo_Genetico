4d6d439786b6c701158ecb1bcd679b334eb7e608
Matheus <matheus.tosin@edu.pucrs.br>
2021-04-19 17:10:10 -0300

    ajuste variaveis

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,29 @@ import java.io.*;
 
 public class Main {
     private static final int TAM = 11;
-    private static File dir = new File("C:\\Users\\matheus.tosin\\Desktop\\PUCRS\\IA\\Algoritmo_Genetico");
-    private static  File arq = new File(dir, "duplos50.txt");
+    private static final File DIR = new File("C:\\Users\\matheus.tosin\\Desktop\\PUCRS\\IA\\Algoritmo_Genetico");
+    private static final File ARQ = new File(DIR, "duplos50.txt");
     private static String[][] preferencias;
 
     public static void main(String[] args) throws FileNotFoundException {
-        carregaTxt(arq);
+        init();
+    }
 
+    public static void init() throws FileNotFoundException {
+        carregaTxt(ARQ);
         printMatriz();
     }
 
-    public static void printMatriz() {
-        for(int i = 0; i < (preferencias.length); i++) {
-            for(int j = 0; j < (preferencias.length/2)+1; j++) {
-                System.out.print(preferencias[i][j] + " ");
+    private static void printMatriz() {
+        for (String[] preferencia : preferencias) {
+            for (int j = 0; j < (preferencias.length / 2) + 1; j++) {
+                System.out.print(preferencia[j] + " ");
             }
             System.out.println();
         }
     }
 
-    public static void carregaTxt(File arq) throws FileNotFoundException {
+    private static void carregaTxt(File arq) throws FileNotFoundException {
         try {
             int lin = 0;
             FileReader fileReader = new FileReader(arq);
