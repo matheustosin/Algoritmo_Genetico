a300b0ffa8739a0a15daff975cbebb0dde38b274
Matheus <matheus.tosin@edu.pucrs.br>
2021-04-20 21:10:26 -0300

    calculo de aptidao

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,28 @@ public class Main {
     private static final int TAM = 11;
     private static int index;
     private static final File DIR = new File("C:\\Users\\matheus.tosin\\Desktop\\PUCRS\\IA\\Algoritmo_Genetico");
-    private static final File ARQ = new File(DIR, "duplos4.txt");
+    private static final File ARQ = new File(DIR, "duplos4ideal.txt");
     private static String[][] preferences;
     private static int[][] population;
+    //private static int[][] intermediaria;
 
     public static void main(String[] args) throws FileNotFoundException {
         init();
         population();
-        printMatrix();
+        //System.out.println("==========================================================================================================================================");
+
+        for (int g=0; g<300; g++) {
+            System.out.println("Geração: " + g);
+            aptidao();
+            printMatrix();
+        }
     }
 
     public static void population() {
         Random rand = new Random();
-        population = new int[TAM][index];
+        population = new int[TAM][index+1];
         for(int i = 0; i < population.length; i++) {
-            List<Integer> aux = new ArrayList<>(3);
+            List<Integer> aux = new ArrayList<>();
             for(int j = 0; j < index; j++) {
                 int nextVal = rand.nextInt(index)+1;
                 if(!aux.contains(nextVal)){
@@ -36,7 +43,8 @@ public class Main {
 
     public static void printMatrix() {
         for(int i = 0; i < TAM; i++) {
-            for(int j = 0; j < index; j++) {
+            System.out.print("C: ");
+            for(int j = 0; j < index+1; j++) {
                     System.out.print(population[i][j] + " ");
             }
             System.out.println();
@@ -45,8 +53,7 @@ public class Main {
 
     public static void init() throws FileNotFoundException {
         loadTxt(ARQ);
-        printMatrixPreferencia();
-        System.out.println("==============================================");
+        //rintMatrixPreferencia();
     }
 
     private static void printMatrixPreferencia() {
@@ -78,6 +85,30 @@ public class Main {
                 bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void aptidao() {
+        //percorre as linhas da populacao
+        for(int i = 0; i < TAM; i++) {
+            int somaMatch = 0;
+            //percorre as colunas da populacao
+            for(int j = 0; j < index; j++) {
+                //percorre de uma coluna da preferencia
+                for(int k = 1; k <= index; k++) {
+                    if (population[i][j] == Character.getNumericValue(preferences[j][k].charAt(1))){
+                        somaMatch += k;
+                        break;
+                    }
+                }
+                for(int k = 1; k <= index; k++) {
+                    if (population[i][j] == Character.getNumericValue(preferences[j+index][k].charAt(1))){
+                        somaMatch += k;
+                        break;
+                    }
+                }
+            }
+            population[i][index] = somaMatch;
         }
     }
 }
